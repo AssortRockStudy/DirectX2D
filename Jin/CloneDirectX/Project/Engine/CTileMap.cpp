45bc88538504ece159cabde8ddaaeb8428120b08
woo <woo060874@naver.com>
2024-01-30 00:07:21 +0900

    #18 240122 MON 1

--- Jin/CloneDirectX/Project/Engine/CTileMap.cpp
+++ Jin/CloneDirectX/Project/Engine/CTileMap.cpp
@@ -1,6 +1,8 @@
 #include "pch.h"
 #include "CTileMap.h"
 
+#include "CStructuredBuffer.h"
+
 #include "CAssetMgr.h"
 #include "CMesh.h"
 
@@ -12,13 +14,18 @@ CTileMap::CTileMap()
 	, m_FaceY(2)
 	, m_vTileRenderSize(Vec2(128.f, 128.f))
 	, m_TileIdx(47)
+	, m_TileInfoBuffer(nullptr)
 {
 	SetMesh(CAssetMgr::GetInst()->FindAsset<CMesh>(L"RectMesh"));
 	SetMaterial(CAssetMgr::GetInst()->FindAsset<CMaterial>(L"TileMapMtrl"));
+	m_TileInfoBuffer = new CStructuredBuffer;
+	SetFace(m_FaceX, m_FaceY);
 }
 
 CTileMap::~CTileMap()
 {
+	if (nullptr != m_TileInfoBuffer)
+		delete m_TileInfoBuffer;
 }
 
 
@@ -29,6 +36,38 @@ void CTileMap::SetTileAtlas(Ptr<CTexture> _Atlas, Vec2 _TilePixelSize)
 
 	m_MaxCol = m_TileAtlas->GetWidth() / (UINT)m_vTilePixelSize.x;
 	m_MaxRow = m_TileAtlas->GetHeight() / (UINT)m_vTilePixelSize.y;
+
+	m_vSliceSizeUV = Vec2(m_vTilePixelSize.x / m_TileAtlas->GetWidth()
+		, m_vTilePixelSize.y / m_TileAtlas->GetHeight());
+}
+
+void CTileMap::SetFace(UINT _FaceX, UINT _FaceY)
+{
+	m_FaceX = _FaceX;
+	m_FaceY = _FaceY;
+
+	vector<tTileInfo> vecTemp;
+	m_vecTileInfo.swap(vecTemp);
+	m_vecTileInfo.resize(_FaceX * _FaceY);
+
+	m_TileInfoBuffer->Create(sizeof(tTileInfo), _FaceX * _FaceY, SB_TYPE::READ_ONLY, true);
+}
+
+void CTileMap::SetTileIndex(UINT _Row, UINT _Col, UINT _ImgIdx)
+{
+	if (nullptr == m_TileAtlas)
+		return;
+
+	UINT idx = _Row * m_FaceX + _Col;
+
+	// 렌더링할 타일 정보
+	UINT iRow = _ImgIdx / m_MaxCol;
+	UINT iCol = _ImgIdx % m_MaxCol;
+
+	m_vecTileInfo[idx].vLeftTopUV = Vec2((iCol * m_vTilePixelSize.x) / m_TileAtlas->GetWidth()
+		, (iRow * m_vTilePixelSize.y) / m_TileAtlas->GetHeight());
+
+	m_vecTileInfo[idx].bRender = 1;
 }
 
 void CTileMap::finaltick()
