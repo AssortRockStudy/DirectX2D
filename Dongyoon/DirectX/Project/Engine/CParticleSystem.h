856ed69122c235124b33da3a34a0cab0e4ef98a4
SEO DONG YUN <skdy1201@gmail.com>
2024-02-28 05:25:32 +0900

    #18
    24.01.22
    
    타일맵 컴포넌트 및 파티클 시작

---
+++ Dongyoon/DirectX/Project/Engine/CParticleSystem.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "CRenderComponent.h"
+
+class CParticleSystem :
+    public CRenderComponent
+{
+private:
+
+public:
+    CParticleSystem();
+    ~CParticleSystem();
+};
+
---
+++ Dongyoon/DirectX/Project/Engine/unity_AQJA6CZHCJXWN3D7.cpp
@@ -0,0 +1,67 @@
+#include "pch.h"
+
+#include "C:\Users\skdy1\OneDrive\Desktop\Study\Direct\Directx\Project\Engine\CLight2D.cpp"
+
+
+#include "C:\Users\skdy1\OneDrive\Desktop\Study\Direct\Directx\Project\Engine\CMaterial.cpp"
+
+
+#include "C:\Users\skdy1\OneDrive\Desktop\Study\Direct\Directx\Project\Engine\CMesh.cpp"
+
+
+#include "C:\Users\skdy1\OneDrive\Desktop\Study\Direct\Directx\Project\Engine\CMeshRender.cpp"
+
+
+#include "C:\Users\skdy1\OneDrive\Desktop\Study\Direct\Directx\Project\Engine\CMissileScript.cpp"
+
+
+#include "C:\Users\skdy1\OneDrive\Desktop\Study\Direct\Directx\Project\Engine\CParticleSystem.cpp"
+
+
+#include "C:\Users\skdy1\OneDrive\Desktop\Study\Direct\Directx\Project\Engine\CPathMgr.cpp"
+
+
+#include "C:\Users\skdy1\OneDrive\Desktop\Study\Direct\Directx\Project\Engine\CPlayerScript.cpp"
+
+
+#include "C:\Users\skdy1\OneDrive\Desktop\Study\Direct\Directx\Project\Engine\CRenderComponent.cpp"
+
+
+#include "C:\Users\skdy1\OneDrive\Desktop\Study\Direct\Directx\Project\Engine\CRenderMgr.cpp"
+
+
+#include "C:\Users\skdy1\OneDrive\Desktop\Study\Direct\Directx\Project\Engine\CRenderMgr_Init.cpp"
+
+
+#include "C:\Users\skdy1\OneDrive\Desktop\Study\Direct\Directx\Project\Engine\CScript.cpp"
+
+
+#include "C:\Users\skdy1\OneDrive\Desktop\Study\Direct\Directx\Project\Engine\CShader.cpp"
+
+
+#include "C:\Users\skdy1\OneDrive\Desktop\Study\Direct\Directx\Project\Engine\CStructuredBuffer.cpp"
+
+
+#include "C:\Users\skdy1\OneDrive\Desktop\Study\Direct\Directx\Project\Engine\CTexture.cpp"
+
+
+#include "C:\Users\skdy1\OneDrive\Desktop\Study\Direct\Directx\Project\Engine\CTileMap.cpp"
+
+
+#include "C:\Users\skdy1\OneDrive\Desktop\Study\Direct\Directx\Project\Engine\CTimeMgr.cpp"
+
+
+#include "C:\Users\skdy1\OneDrive\Desktop\Study\Direct\Directx\Project\Engine\CTransform.cpp"
+
+
+#include "C:\Users\skdy1\OneDrive\Desktop\Study\Direct\Directx\Project\Engine\Engine.cpp"
+
+
+#include "C:\Users\skdy1\OneDrive\Desktop\Study\Direct\Directx\Project\Engine\extern.cpp"
+
+
+#include "C:\Users\skdy1\OneDrive\Desktop\Study\Direct\Directx\Project\Engine\func.cpp"
+
+
+#include "C:\Users\skdy1\OneDrive\Desktop\Study\Direct\Directx\Project\Engine\CTaskMgr.cpp"
+
